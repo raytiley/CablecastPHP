782b391da6fef320932c37d47c2aec6ffab7ba54
Ray Tiley <raytiley@gmail.com>
2013-02-10 18:20:28 -0600

    Forgot comma on namespace decloration

---
+++ Show.php
@@ -0,0 +1,39 @@
+<?php
+namespace CablecastPHP;
+
+class Show
+{
+    function __construct($showID, $title, $cgTitle, $comments, $lengthInSeconds)
+    {
+        $this->showID = $showID;
+        $this->title = $title;
+        $this->cgTitle = $cgTitle;
+        $this->comments = $comments;
+        $this->lengthInSeconds = $lengthInSeconds;
+    }
+
+  private $title;
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  private $cgTitle;
+  public function getCGTitle()
+  {
+    return $this->cgTitle;
+  }
+
+  private $lengthInSeconds;
+  public function getLengthInSeconds()
+  {
+    return $this->lengthInSeconds;
+  }
+
+  private $comments;
+  public function getComments()
+  {
+    return $this->comments;
+  }
+}
+
